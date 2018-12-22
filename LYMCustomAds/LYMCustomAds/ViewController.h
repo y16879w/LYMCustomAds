fd520d4e4c48e62261db049456659e148035a194
liyanmin <liyanmin3@jd.com>
2018-12-22 14:42:58 +0800

    commit

---
+++ LYMCustomAds/LYMCustomAds/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LYMCustomAds
+//
+//  Created by 李艳敏 on 2018/12/22.
+//  Copyright © 2018年 李艳敏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LYMCustomAds/LYMCustomAds/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LYMCustomAds
+//
+//  Created by 李艳敏 on 2018/12/22.
+//  Copyright © 2018年 李艳敏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
